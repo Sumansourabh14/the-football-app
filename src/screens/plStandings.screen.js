86cf4c0d087631dd28640f5825d310cbf20643e1
Suman Sourabh <sumsourabh14@gmail.com>
2023-02-26 17:42:19 +0530

    added table clubs

--- src/screens/plStandings.screen.js
+++ src/screens/plStandings.screen.js
@@ -2,16 +2,48 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import tableData from "../content/plStandings.json";
 import { fontSizes } from "../styles/fonts";
+import { SvgUri } from "react-native-svg";
 
 const PlStandings = () => {
   return (
-    <View>
+    <View
+      style={{
+        paddingTop: 600,
+      }}
+    >
       <Image
         source={{ uri: tableData.competition.emblem }}
         style={styles.leagueEmblem}
       />
       <Text style={styles.leagueTitle}>{tableData.competition.name}</Text>
       <Text style={styles.leagueCountry}>{tableData.area.name}</Text>
+
+      <View>
+        {tableData.standings[0].type === "TOTAL" ? (
+          tableData.standings[0].table.map((team) => (
+            <View>
+              <Text>{team.position}</Text>
+              <Text>{team.team.name}</Text>
+              {team.team.crest.includes(".svg") ? (
+                <SvgUri
+                  uri={team.team.crest}
+                  width={30}
+                  height={30}
+                  style={{ marginHorizontal: 0 }}
+                />
+              ) : (
+                <Image
+                  source={{ uri: team.team.crest }}
+                  style={styles.crest}
+                  alt={team.team.name}
+                />
+              )}
+            </View>
+          ))
+        ) : (
+          <Text></Text>
+        )}
+      </View>
     </View>
   );
 };
@@ -29,5 +61,10 @@ const styles = StyleSheet.create({
   leagueEmblem: {
     height: 100,
     width: 100,
+  },
+  crest: {
+    height: 30,
+    width: 30,
+    marginHorizontal: 6,
   },
 });
