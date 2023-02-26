9e2ac34aaa746059ecaa3917be9b9c95f5906bd4
Suman Sourabh <sumsourabh14@gmail.com>
2023-02-27 00:40:15 +0530

    added flex layout to table

--- src/screens/plStandings.screen.js
+++ src/screens/plStandings.screen.js
@@ -1,16 +1,13 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
 import tableData from "../content/plStandings.json";
 import { fontSizes } from "../styles/fonts";
 import { SvgUri } from "react-native-svg";
+const { width, height } = Dimensions.get("window");
 
 const PlStandings = () => {
   return (
-    <View
-      style={{
-        paddingTop: 300,
-      }}
-    >
+    <View style={[styles.container, { marginTop: 360 }]}>
       <Image
         source={{ uri: tableData.competition.emblem }}
         style={styles.leagueEmblem}
@@ -18,31 +15,47 @@ const PlStandings = () => {
       <Text style={styles.leagueTitle}>{tableData.competition.name}</Text>
       <Text style={styles.leagueCountry}>{tableData.area.name}</Text>
 
-      <View>
+      <View style={[styles.standings]}>
         {tableData.standings[0].type === "TOTAL" ? (
           tableData.standings[0].table.map((team) => (
-            <View style={[styles.clubPositionLayout, styles.clubSpacing]}>
-              <Text>{team.position}</Text>
-              {team.team.crest.includes(".svg") ? (
-                <SvgUri
-                  uri={team.team.crest}
-                  width={30}
-                  height={30}
-                  style={{ marginHorizontal: 0 }}
-                />
-              ) : (
-                <Image
-                  source={{ uri: team.team.crest }}
-                  style={styles.crest}
-                  alt={team.team.name}
-                />
-              )}
-              <Text>{team.team.shortName}</Text>
-              <Text>{team.playedGames}</Text>
-              <Text>{team.won}</Text>
-              <Text>{team.draw}</Text>
-              <Text>{team.lost}</Text>
-              <Text>{team.points}</Text>
+            <View
+              key={team.position}
+              style={[styles.clubPositionLayout, styles.clubSpacing]}
+            >
+              <View style={{ flexDirection: "row", flex: 2.5 }}>
+                <Text
+                  style={{ marginRight: 10, width: 20, textAlign: "center" }}
+                >
+                  {team.position}
+                </Text>
+                {team.team.crest.includes(".svg") ? (
+                  <SvgUri
+                    uri={team.team.crest}
+                    width={30}
+                    height={30}
+                    style={{ marginRight: 10 }}
+                  />
+                ) : (
+                  <Image
+                    source={{ uri: team.team.crest }}
+                    style={styles.crest}
+                    alt={team.team.name}
+                  />
+                )}
+                <Text>{team.team.shortName}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  flex: 1.5,
+                }}
+              >
+                <Text style={styles.clubStats}>{team.playedGames}</Text>
+                <Text style={styles.clubStats}>{team.won}</Text>
+                <Text style={styles.clubStats}>{team.draw}</Text>
+                <Text style={styles.clubStats}>{team.lost}</Text>
+                <Text style={[styles.clubStats]}>{team.points}</Text>
+              </View>
             </View>
           ))
         ) : (
@@ -59,6 +72,16 @@ const styles = StyleSheet.create({
   clubPositionLayout: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  container: {
+    // width: width,
+    // height: height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  standings: {
+    width: 360,
+    padding: 10,
   },
   clubSpacing: {
     marginBottom: 20,
@@ -77,6 +100,16 @@ const styles = StyleSheet.create({
   crest: {
     height: 30,
     width: 30,
-    marginHorizontal: 6,
+    marginRight: 10,
+  },
+  border: {
+    borderColor: "#000",
+    borderStyle: "solid",
+    borderWidth: 2,
+  },
+  clubStats: {
+    marginLeft: 8,
+    width: 18,
+    textAlign: "center",
   },
 });
