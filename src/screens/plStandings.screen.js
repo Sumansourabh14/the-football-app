1542040d78efdd37ef899756484a87668569a0e3
Suman Sourabh <sumsourabh14@gmail.com>
2023-03-01 00:04:34 +0530

    added standings table header

--- src/screens/plStandings.screen.js
+++ src/screens/plStandings.screen.js
@@ -1,68 +1,99 @@
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SvgUri } from "react-native-svg";
 import tableData from "../content/plStandings.json";
 import { fontSizes } from "../styles/fonts";
-import { SvgUri } from "react-native-svg";
 const { width, height } = Dimensions.get("window");
 
 const PlStandings = () => {
   return (
-    <View style={[styles.container, { marginTop: 360 }]}>
-      <Image
-        source={{ uri: tableData.competition.emblem }}
-        style={styles.leagueEmblem}
-      />
-      <Text style={styles.leagueTitle}>{tableData.competition.name}</Text>
-      <Text style={styles.leagueCountry}>{tableData.area.name}</Text>
-
-      <View style={[styles.standings, { marginTop: 20 }]}>
-        {tableData.standings[0].type === "TOTAL" ? (
-          tableData.standings[0].table.map((team) => (
-            <View
-              key={team.position}
-              style={[styles.clubPositionLayout, styles.clubSpacing]}
-            >
-              <View style={{ flexDirection: "row", flex: 2.5 }}>
-                <Text
-                  style={{ marginRight: 10, width: 20, textAlign: "center" }}
-                >
-                  {team.position}
-                </Text>
-                {team.team.crest.includes(".svg") ? (
-                  <SvgUri
-                    uri={team.team.crest}
-                    width={30}
-                    height={30}
-                    style={{ marginRight: 10 }}
-                  />
-                ) : (
-                  <Image
-                    source={{ uri: team.team.crest }}
-                    style={styles.crest}
-                    alt={team.team.name}
-                  />
-                )}
-                <Text>{team.team.shortName}</Text>
-              </View>
+    <ScrollView>
+      <View style={[styles.container, { marginTop: 0 }]}>
+        <Image
+          source={{ uri: tableData.competition.emblem }}
+          style={styles.leagueEmblem}
+        />
+        <Text style={styles.leagueTitle}>{tableData.competition.name}</Text>
+        <Text style={styles.leagueCountry}>{tableData.area.name}</Text>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: 360,
+            backgroundColor: "#E5E5CB",
+            paddingVertical: 4,
+          }}
+        >
+          <View style={{ flexDirection: "row", flex: 2.5 }}>
+            {/* <Text style={styles.clubStats}>P</Text>
+              <Text style={{ textAlign: "center", width: "100%" }}>Club</Text> */}
+          </View>
+          <View style={{ flexDirection: "row", flex: 1.55 }}>
+            <Text style={styles.clubStats}>P</Text>
+            <Text style={styles.clubStats}>W</Text>
+            <Text style={styles.clubStats}>D</Text>
+            <Text style={styles.clubStats}>L</Text>
+            <Text style={styles.clubStats}>Pt</Text>
+          </View>
+        </View>
+        <View style={[styles.standings, { marginTop: 10 }]}>
+          {tableData.standings[0].type === "TOTAL" ? (
+            tableData.standings[0].table.map((team) => (
               <View
-                style={{
-                  flexDirection: "row",
-                  flex: 1.5,
-                }}
+                key={team.position}
+                style={[styles.clubPositionLayout, styles.clubSpacing]}
               >
-                <Text style={styles.clubStats}>{team.playedGames}</Text>
-                <Text style={styles.clubStats}>{team.won}</Text>
-                <Text style={styles.clubStats}>{team.draw}</Text>
-                <Text style={styles.clubStats}>{team.lost}</Text>
-                <Text style={[styles.clubStats]}>{team.points}</Text>
+                <View style={{ flexDirection: "row", flex: 2.5 }}>
+                  <Text
+                    style={{ marginRight: 10, width: 20, textAlign: "center" }}
+                  >
+                    {team.position}
+                  </Text>
+                  {team.team.crest.includes(".svg") ? (
+                    <SvgUri
+                      uri={team.team.crest}
+                      width={30}
+                      height={30}
+                      style={{ marginRight: 10 }}
+                    />
+                  ) : (
+                    <Image
+                      source={{ uri: team.team.crest }}
+                      style={styles.crest}
+                      alt={team.team.name}
+                    />
+                  )}
+                  <Text>{team.team.shortName}</Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    flex: 1.5,
+                  }}
+                >
+                  <Text style={styles.clubStats}>{team.playedGames}</Text>
+                  <Text style={styles.clubStats}>{team.won}</Text>
+                  <Text style={styles.clubStats}>{team.draw}</Text>
+                  <Text style={styles.clubStats}>{team.lost}</Text>
+                  <Text style={[styles.clubStats]}>{team.points}</Text>
+                </View>
               </View>
-            </View>
-          ))
-        ) : (
-          <Text></Text>
-        )}
+            ))
+          ) : (
+            <Text></Text>
+          )}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
